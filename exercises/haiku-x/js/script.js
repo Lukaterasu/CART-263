50349beec35a2ba16e34ac537dca5295dabc4ac8
Lukaterasu <lukaross06@gmail.com>
2021-03-13 19:40:00 -0500

    done

---
+++ exercises/haiku-x/js/script.js
@@ -0,0 +1,40 @@
+
+let poems = ["one","two","three","four","five", "six"];
+
+let ps = document.querySelectorAll("p");
+
+for (let i = 0; i < ps.length; i++) {
+
+    const element = ps[i];
+    let rand = Math.floor(Math.random() * poems.length);
+    element.innerHTML = poems[rand];
+    poems.splice(rand, 1);
+    
+    element.addEventListener("click", () => {
+    
+        element.classList.remove("fadeIn");
+        element.classList.remove("shake");
+       element.classList.add("fadeOut");
+
+    });
+    element.addEventListener("animationend", endFade);
+
+   
+}
+
+function endFade(){
+    let rand = Math.floor(Math.random() * poems.length);
+    this.innerHTML = poems[rand];
+    poems.splice(rand, 1);
+    if(poems.length <=0){
+     poems = ["one","two","three","four","five", "six"];
+    }
+    this.classList.remove("fadeOut");
+    this.classList.add("fadeIn");
+    this.removeEventListener("animationend",endFade);
+    this.addEventListener("animationend", ()=> {
+        this.classList.add("shake");
+        this.addEventListener("animationend", endFade);
+    });
+
+}
